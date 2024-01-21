3782d564172dafd47859977cb6d28a15de492bba
jorryns <jorryn.salvador@gmail.com>
2024-01-20 18:03:40 -0800

    alpha build 1.081
    -added some stuff

--- scenes/schoolInterior.gd
+++ scenes/schoolInterior.gd
@@ -5,3 +5,8 @@ var player = player_scene.instantiate()
 
 func _ready():
 	self.add_child(player)
+
+
+func _on_portal_to_school_yard_body_entered(body):
+	if body.name == "jorryn":
+		SceneManager.change_scene("res://scenes/schoolyard.tscn")
